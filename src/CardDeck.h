8ba114a443233b058d0cc990a87f6b03a87fa281
MatinQurban <matinqurban@gmail.com>
2023-09-13 15:36:37 -0700

    Header file for CardDeck class

---
+++ src/CardDeck.h
@@ -0,0 +1,33 @@
+// Header file for deck of cards class.
+// Description: This class represents a deck of cards. It has an array of cards and can perfectly shuffle them.
+
+#ifndef CARDDECK_H
+#define CARDDECK_H
+
+
+#include "Card.h"
+
+
+class CardDeck
+{
+public:
+    CardDeck();
+    CardDeck(Card c[]);
+
+    CardDeck perfectShuffle();
+    void printDeck();
+    bool operator==(const CardDeck& otherDeck) const;
+
+private:
+    Card deck[52];
+
+}
+
+
+
+
+
+
+
+
+#endif //CARDDECK_H
