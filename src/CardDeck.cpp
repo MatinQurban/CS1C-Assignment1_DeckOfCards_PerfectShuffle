c68bb774033c7d55b968e32cb6e63144bf6a7b48
MatinQurban <matinqurban@gmail.com>
2023-09-13 17:18:52 -0700

    Begining to define CardDeck class

--- src/CardDeck.cpp
+++ src/CardDeck.cpp
@@ -18,3 +18,49 @@ CardDeck::CardDeck()
     
 
 }
+
+CardDeck::CardDeck(Card c[])
+{
+    for(int i = 0; i < 52; i++)
+    {
+        deck[i] = c[i];
+    }
+}
+
+CardDeck CardDeck::perfectShuffle()
+{
+    CardDeck shuffledDeck;
+    int deckPos = 0;
+    for(int i = 0; i < 26; i++)
+    {
+        shuffledDeck.deck[deckPos] = deck[i];
+        deckPos += 2;
+    }
+    deckPos = 1;
+    for(int i = 26; i < 52; i++)
+    {
+        shuffledDeck.deck[deckPos] = deck[i];
+        deckPos += 2;
+    }
+    return shuffledDeck;
+}
+
+void CardDeck::printDeck()
+{
+    for(int i = 0; i < 52; i++)
+    {
+        deck[i].printCard();
+    }
+}
+
+bool CardDeck::operator==(const CardDeck& otherDeck) const
+{
+    for(int i = 0; i < 52; i++)
+    {
+        if(deck[i].getSuit() != otherDeck.deck[i].getSuit() || deck[i].getRank() != otherDeck.deck[i].getRank())
+        {
+            return false;
+        }
+    }
+    return true;
+}
